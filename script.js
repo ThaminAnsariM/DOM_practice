9e1c6de2d61aaaccca5930ca28bc4240796221e9
ThaminAnsariM <117235590+ThaminAnsariM@users.noreply.github.com>
2023-03-13 12:24:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function foo(){
+    var first = document.getElementById("Firstname").value;
+    console.log(first)
+
+    var middle = document.getElementById("middlename").value;
+    console.log(middle)
+
+    var last = document.getElementById("lastname").value;
+    console.log(last)
+
+    var email = document.getElementById("email").value;
+    console.log(email)
+
+    var pwd = document.getElementById("pwd").value;
+    console.log(pwd)
+}
